1db85fd005837819fa70a864604f36fb70fe2b33
Matgc <92761383+Matgc04@users.noreply.github.com>
2024-05-02 12:15:44 -0300

    campos agregados al registrar

--- YateMate/Data/ApplicationUser.cs
+++ YateMate/Data/ApplicationUser.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNetCore.Identity;
 
 namespace YateMate.Data;
@@ -5,4 +6,17 @@ namespace YateMate.Data;
 // Add profile data for application users by adding properties to the ApplicationUser class
 public class ApplicationUser : IdentityUser
 {
+    [MaxLength(100)]
+    public string? Nombre { get; set; }
+    [MaxLength(100)]
+    public string? Apellido { get; set; }
+    public DateTime? FechaNacimiento { get; set; }
+
+    [MaxLength(9)] //Masculino = 9 letras, sacar esto
+    public string? Genero { get; set; }
+    
+    public int? Dni { get; set; }
+    
+    [MaxLength(100)]
+    public string? Nacionalidad { get; set; }  //no se esto que tipo hacerlo
 }
---
+++ YateMate/Data/Migrations/20240502134527_datos-registro.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace YateMate.Migrations
+{
+    /// <inheritdoc />
+    public partial class datosregistro : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Apellido",
+                table: "AspNetUsers",
+                type: "TEXT",
+                maxLength: 100,
+                nullable: true);
+
+            migrationBuilder.AddColumn<uint>(
+                name: "Dni",
+                table: "AspNetUsers",
+                type: "INTEGER",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "FechaNacimiento",
+                table: "AspNetUsers",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Nacionalidad",
+                table: "AspNetUsers",
+                type: "TEXT",
+                maxLength: 100,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Nombre",
+                table: "AspNetUsers",
+                type: "TEXT",
+                maxLength: 100,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Apellido",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Dni",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "FechaNacimiento",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Nacionalidad",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Nombre",
+                table: "AspNetUsers");
+        }
+    }
+}
