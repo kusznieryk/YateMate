516aebaef3a0c34f5a712f1fd278ee791ba382ea
manu-peralta <manuperalta02@hotmail.com>
2024-05-05 20:55:46 -0300

    Creacion de repositorios de bien y oferta

---
+++ YateMate.Repositorio/RepositorioBien.cs
@@ -0,0 +1,60 @@
+using System.Reflection.Metadata.Ecma335;
+using YateMate.Aplicacion.Entidades;
+using YateMate.Aplicacion.Interfaces;
+namespace YateMate.Repositorio;
+
+public class RepositorioBien : IRepositorioBien
+{
+    public void AgregarBien(Bien bien)
+    {
+        using (var context = new ApplicationDbContext())
+        {
+            context.Add(bien);
+            context.SaveChanges();
+        }
+    }
+
+    public Bien? ObtenerBien(int id)
+    {
+        using (var context = new ApplicationDbContext())
+        {
+            return context.Bienes.FirstOrDefault(b => b.Id == id);
+        }
+    }
+
+    public void ModificarBien(Bien bien)
+    {
+        using (var context = new ApplicationDbContext())
+        {
+            var bienViejo = context.Bienes.FirstOrDefault(b => b.Id == bien.Id);
+            if (bienViejo != null)
+            {
+                bienViejo.Nombre = bien.Nombre;
+                bienViejo.Descripcion = bien.Descripcion;
+                bienViejo.Imagen = bien.Imagen;
+                context.SaveChanges();
+            }
+        }
+    }
+
+    public void EliminarBien(int id)
+    {
+        using (var context = new ApplicationDbContext())
+        {
+            var bien = context.Bienes.FirstOrDefault(b => b.Id == id);
+            if (bien != null)
+            {
+                context.Remove(bien);
+                context.SaveChanges();
+            }
+        }
+    }
+
+    public List<Bien> ListarBienesDe(int id)
+    {
+        using (var context = new ApplicationDbContext())
+        {
+            return context.Bienes.Where(b => b.UsuarioId == id).ToList();
+        }
+    }
+}
