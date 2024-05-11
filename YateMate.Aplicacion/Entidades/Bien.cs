3509b484c32e62ef7022e1409db8a5642c38a759
manu-peralta <manuperalta02@hotmail.com>
2024-05-11 20:49:42 -0300

    hice que el id del bien se ponga automaticamente y cambie todos los tipos de los atributos a string(hay que cambiar el de imagen)

--- YateMate.Aplicacion/Entidades/Bien.cs
+++ YateMate.Aplicacion/Entidades/Bien.cs
@@ -5,14 +5,14 @@ namespace YateMate.Aplicacion.Entidades;
 
 public class Bien
 {
-    public int Id { get; set; }
+    public int Id { get; set; } = Guid.NewGuid().GetHashCode();
     [MaxLength(50)]
-    public String Nombre { get; set; }
-    public String Descripcion { get; set; }
-    public int UsuarioId { get; set; }
-    public byte[] Imagen { get; set; }
+    public string Nombre { get; set; }
+    public string Descripcion { get; set; }
+    public string UsuarioId { get; set; }
+    public string Imagen { get; set; }
 
-    public Bien(string nombre, string descripcion, byte[] imagen, int usuarioId)
+    public Bien(string nombre, string descripcion, string imagen, string usuarioId)
     {
         this.Nombre = nombre;
         this.Descripcion = descripcion;
