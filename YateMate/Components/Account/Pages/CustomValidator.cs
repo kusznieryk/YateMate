a0a46d1656e7e87e50936d6a7eeaf939d244e2de
Matgc <92761383+Matgc04@users.noreply.github.com>
2024-05-15 14:10:48 -0300

    Saque algunos botones del nav, hice checkeo que salga bien en registrar y en modificar perfil (pero ahi esta roto)

---
+++ YateMate/Components/Account/Pages/CustomValidator.cs
@@ -0,0 +1,34 @@
+using System.Runtime.InteropServices.JavaScript;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+
+namespace YateMate.Components.Account.Pages;
+
+public class CustomValidator : ComponentBase
+{
+    private ValidationMessageStore _messageStore;
+
+    [CascadingParameter]
+    public EditContext CurrentEditContext { get; set; } = null!;
+
+    protected override void OnInitialized()
+    {
+        if (CurrentEditContext == null)
+        {
+            throw new InvalidOperationException("To use validator component your razor page should have the edit component");
+        }
+        _messageStore = new ValidationMessageStore(CurrentEditContext);
+        CurrentEditContext.OnValidationRequested += (s, e) => _messageStore.Clear();
+        CurrentEditContext.OnFieldChanged += (s, e) => _messageStore.Clear(e.FieldIdentifier);
+    }
+
+    public void DisplayErrors(Dictionary<string, List<string>> errors)
+    {
+        foreach (var error in errors)
+        {
+            _messageStore.Add(CurrentEditContext.Field("a"), "b");
+            _messageStore.Add(CurrentEditContext.Field(error.Key), error.Value);
+        }
+        CurrentEditContext.NotifyValidationStateChanged();
+    }
+}
