dd14111ce71ffdde523210821dc4b179451048e9
Matgc <92761383+Matgc04@users.noreply.github.com>
2024-05-27 14:02:04 -0300

    Se pueden enviar imagenes por chat, siempre y cuando sean menores a 32KB

---
+++ YateMate.Repositorio/Migrations/20240527153939_ImagenChat.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace YateMate.Repositorio.Migrations
+{
+    /// <inheritdoc />
+    public partial class ImagenChat : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsImage",
+                table: "MensajesChats",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsImage",
+                table: "MensajesChats");
+        }
+    }
+}
