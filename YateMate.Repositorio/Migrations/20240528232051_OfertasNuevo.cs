44e33d8f809db556e679ffa8e9c341c4d5dc8e5d
manu-peralta <manuperalta02@hotmail.com>
2024-05-28 21:36:36 -0300

    Migracion nueva de oferta por nuevo atributo

---
+++ YateMate.Repositorio/Migrations/20240528232051_OfertasNuevo.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace YateMate.Repositorio.Migrations
+{
+    /// <inheritdoc />
+    public partial class OfertasNuevo : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "UsuarioId",
+                table: "Ofertas",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UsuarioId",
+                table: "Ofertas");
+        }
+    }
+}
