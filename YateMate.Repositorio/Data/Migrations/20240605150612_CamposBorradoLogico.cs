321e7afe1d1385a663506b875bf5aaa66eac975a
juanv4 <juanignaciovergara14@gmail.com>
2024-06-05 20:25:40 -0300

    Migraciones

---
+++ YateMate.Repositorio/Data/Migrations/20240605150612_CamposBorradoLogico.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace YateMate.Repositorio.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class CamposBorradoLogico : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Acordado",
+                table: "Ofertas");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "EstaEliminado",
+                table: "Publicaciones",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "EstaEliminado",
+                table: "Bienes",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EstaEliminado",
+                table: "Publicaciones");
+
+            migrationBuilder.DropColumn(
+                name: "EstaEliminado",
+                table: "Bienes");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Acordado",
+                table: "Ofertas",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
