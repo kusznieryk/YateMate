0cf27bde70385fe0d662749881099505a261847d
Matgc <92761383+Matgc04@users.noreply.github.com>
2024-07-01 16:16:55 -0300

    Avance en listados y correccion de entidades en la BD

---
+++ YateMate.Repositorio/Migrations/20240701191534_CorreccionesSubalquilerYReserva.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace YateMate.Repositorio.Migrations
+{
+    /// <inheritdoc />
+    public partial class CorreccionesSubalquilerYReserva : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IdAlquilante",
+                table: "Subalquileres");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "FechaFin",
+                table: "Reservas",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "FechaInicio",
+                table: "Reservas",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reservas_SubalquilerId",
+                table: "Reservas",
+                column: "SubalquilerId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Reservas_Subalquileres_SubalquilerId",
+                table: "Reservas",
+                column: "SubalquilerId",
+                principalTable: "Subalquileres",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Reservas_Subalquileres_SubalquilerId",
+                table: "Reservas");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Reservas_SubalquilerId",
+                table: "Reservas");
+
+            migrationBuilder.DropColumn(
+                name: "FechaFin",
+                table: "Reservas");
+
+            migrationBuilder.DropColumn(
+                name: "FechaInicio",
+                table: "Reservas");
+
+            migrationBuilder.AddColumn<string>(
+                name: "IdAlquilante",
+                table: "Subalquileres",
+                type: "TEXT",
+                nullable: true);
+        }
+    }
+}
