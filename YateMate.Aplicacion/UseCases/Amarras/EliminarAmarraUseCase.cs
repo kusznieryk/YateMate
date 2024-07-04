183e1af49dbab4b110d343029092d644e17e66ce
Kusznieryk Eduardo <ekusznieryk0@gmail.com>
2024-07-03 21:43:07 -0300

    cambiando como se muestran las medidas de la amarra

--- YateMate.Aplicacion/UseCases/Amarras/EliminarAmarraUseCase.cs
+++ YateMate.Aplicacion/UseCases/Amarras/EliminarAmarraUseCase.cs
@@ -1,12 +1,15 @@
 using YateMate.Aplicacion.Entidades;
 using YateMate.Aplicacion.Interfaces;
+using YateMate.Aplicacion.UseCases.Subalquiler;
 
 namespace YateMate.Aplicacion.UseCases.Amarras;
 
-public class EliminarAmarraUseCase(IRepositorioAmarra repo)
+public class EliminarAmarraUseCase(IRepositorioAmarra repo, IRepositorioSubalquiler repoSub)
 {
     public void Ejecutar(int idAmarra)
     {
+        var listaDeSubalquileres=repoSub.ObtenerSubalquileresDeLaAmarra(idAmarra);
+        listaDeSubalquileres.ForEach(sub => repoSub.EliminarSubalquiler(sub.Id));
         repo.EliminarAmarra(idAmarra);
     }
 }
